f104b5455750a969e43ac2d51e1366b39487ff82
capitalhealthcare <126751714+capitalhealthcare@users.noreply.github.com>
2023-07-21 07:34:54 +0000

    hydration error fixing

--- pages/conditions-we-treat/car-accident-injury/index.js
+++ pages/conditions-we-treat/car-accident-injury/index.js
@@ -1,14 +1,27 @@
-import { Banner } from "@/components/CommonBanner/Banner";
-import { CommonFormsSections } from "@/components/CommonForms&Sections";
+// import { Banner } from "@/components/CommonBanner/Banner";
+// import { CommonFormsSections } from "@/components/CommonForms&Sections";
+// import { Footer } from "@/components/Footer/Footer";
+// import { Header } from "@/components/Header/Header";
+// import { ServicesSidebarLinks } from "@/components/ServicesSidebarLinks";
+// import { TreatmentsSidebarMenu } from "@/components/TreatmentsSidebarMenu";
+// import Head from "next/head";
+// import Link from "next/link";
+// import React from "react";
+// import { useRouter } from "next/router";
+// import dynamic from 'next/dynamic'
+
 import { Footer } from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
-import { ServicesSidebarLinks } from "@/components/ServicesSidebarLinks";
-import { TreatmentsSidebarMenu } from "@/components/TreatmentsSidebarMenu";
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/router";
 import dynamic from 'next/dynamic'
+
+const Banner = dynamic(() => import("@/components/CommonBanner/Banner"));
+const CommonFormsSections = dynamic(() => import("@/components/CommonForms&Sections"));
+const ServicesSidebarLinks = dynamic(() => import("@/components/ServicesSidebarLinks"));
+const TreatmentsSidebarMenu = dynamic(() => import("@/components/TreatmentsSidebarMenu"));
 
 export const CarAccidentInjury = () => {
   const router = useRouter();
@@ -228,6 +241,4 @@ export const CarAccidentInjury = () => {
   );
 };
 
-export default dynamic(() => Promise.resolve(CarAccidentInjury), {
-  ssr: false,
-})
+export default CarAccidentInjury
